3240e261bd916b8954d91e764dea44185cd76132
Sasha Raimov <crazyysasha@gmail.com>
2022-02-12 15:07:03 +0500

    asd

--- routes.php
+++ routes.php
@@ -3,3 +3,5 @@
 Route::get('chat/{chatId}', [\Crazy\PusherChat\Api\Chat::class, 'getChat']);
 Route::get('chat/{chatId}/messages', [\Crazy\PusherChat\Api\Chat::class, 'getMessages']);
 Route::post('chat/{chatId}/messages', [\Crazy\PusherChat\Api\Chat::class, 'createMessage']);
+
+
